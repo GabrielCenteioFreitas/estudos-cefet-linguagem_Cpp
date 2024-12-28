0f6e57cf0773866a8f3dbb461fa39b3a774dbdea
Gabriel Freitas <gabrielcenteio.freitas@gmail.com>
2024-12-28 18:17:20 -0300

    adicionando desafios de um Editor de Texto e de um Restaurante

---
+++ Projetos/editor-texto.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+#ifdef _WIN32
+  #define CLEAR "cls"
+#else
+  #define CLEAR "clear"
+#endif
+
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define CYAN "\033[36m"
+
+struct no {
+  string conteudo;
+  struct no* prox;
+  struct no* ant;
+};
+typedef struct no* noPtr;
+
+bool listaVazia(noPtr topo) {
+  return !topo;
+}
+
+void printTitle() {
+  printf(BOLD);
+  printf(CYAN);
+  printf("---- Editor de texto ----\n\n");
+  printf(RESET);
+}
+
+void saida() {
+  char caractere;
+  printf(RESET);
+  printf("\nDigite qualquer caractere para voltar ao menu ");
+  cin >> caractere;
+}
+
+void inserir(noPtr* topoEnd, noPtr* fimEnd, string conteudo) {
+  noPtr novoNo = new no;
+  novoNo->conteudo = conteudo;
+  novoNo->ant = *fimEnd;
+  novoNo->prox = NULL;
+  if (listaVazia(*topoEnd)) {
+    *topoEnd = novoNo;
+  } else {
+    (*fimEnd)->prox = novoNo;
+  }
+  *fimEnd = novoNo;
+
+  printf(GREEN);
+  printf("\nLinha inserida com sucesso!\n");
+  saida();
+}
+
+void excluir(noPtr* topoEnd, noPtr* fimEnd, int numLinha) {
+  noPtr aux = *topoEnd;
+
+  if (!(*topoEnd)) {
+    printf(RED);
+    printf("\nO texto nao possui nenhum conteudo.\n");
+    saida();
+    return;
+  }
+
+  if (numLinha < 0) {
+    printf(RED);
+    printf("\nInsira um valor maior ou igual a 0.\n");
+    saida();
+    return;
+  }
+
+  int i = 0;
+  for (i = 0; i < numLinha && aux; i++) {
+    aux = aux->prox;
+  }
+  
+  if (!aux) {
+    printf(RED);
+    printf("O texto possui apenas %d linha(s).\n", i);
+    saida();
+    return;
+  }
+
+  if (aux->ant) aux->ant->prox = aux->prox;
+  if (aux->prox) aux->prox->ant = aux->ant;
+  if (aux == *topoEnd) *topoEnd = aux->prox;
+  if (aux == *fimEnd) *fimEnd = aux->ant;
+
+  printf(GREEN);
+  cout << "\nLinha de conteudo \"" << aux->conteudo << "\" foi excluida!\n";
+  delete aux;
+  saida();
+}
+
+void listar(noPtr topoOuFim, bool mostrarInvertido) {
+  noPtr aux = topoOuFim;
+
+  if (listaVazia(topoOuFim)) {
+    printf(RED);
+    printf("O texto nao possui conteudo.\n");
+    saida();
+    return;
+  }
+
+  while (aux) {
+    cout << aux->conteudo << endl;
+    aux = mostrarInvertido ? aux->ant : aux->prox;
+  }
+
+  saida();
+}
+
+void mover(noPtr* topoEnd, noPtr* fimEnd, int numLinha1, int numLinha2) {
+  noPtr aux1 = *topoEnd, aux2 = *topoEnd, novoNo = new no, temp;
+
+  if (!(*topoEnd)) {
+    printf(RED);
+    printf("\nO texto nao possui nenhum conteudo.\n");
+    saida();
+    return;
+  }
+
+  if (numLinha1 < 0 || numLinha2 < 0) {
+    printf(RED);
+    printf("\nInsira valores maiores ou iguais a 0.\n");
+    saida();
+    return;
+  }
+
+  int i = 0;
+  for (i = 0; i < numLinha1 && aux1; i++) {
+    aux1 = aux1->prox;
+  }
+  for (i = 0; i < numLinha2 && aux2; i++) {
+    aux2 = aux2->prox;
+  }
+  
+  if (!aux1 && !aux2) {
+    printf(RED);
+    printf("\nO texto possui apenas %d linha(s).\n", i);
+    saida();
+    return;
+  }
+  
+  if (!aux1 || !aux2) {
+    novoNo->conteudo = aux1 ? aux1->conteudo : aux2->conteudo;
+    novoNo->ant = *fimEnd;
+    novoNo->prox = NULL;
+    (*fimEnd)->prox = novoNo;
+    *fimEnd = novoNo;
+
+    temp = *topoEnd;
+    if (temp->ant) temp->ant->prox = temp->prox;
+    if (temp->prox) temp->prox->ant = temp->ant;
+    if (temp == *fimEnd) *fimEnd = temp->ant;
+    *topoEnd = temp->prox;
+    delete temp;
+  } else {
+    swap(aux1->conteudo, aux2->conteudo);
+  }
+
+  printf(GREEN);
+  printf("\nLinhas movidas com sucesso!\n");
+  saida();
+}
+
+int menu() {
+  int escolha;
+
+  system(CLEAR);
+  printTitle();
+  cout << "[0] Sair\n"
+       << "[1] Inserir linha\n"
+       << "[2] Excluir linha\n"
+       << "[3] Mostrar texto\n"
+       << "[4] Mostrar texto invertido\n"
+       << "[5] Mover linhas\n\n"
+       << "Qual acao deseja realizar? ";
+  cin >> escolha;
+
+  return escolha;
+}
+
+int main() {
+  noPtr topo = NULL, fim = NULL;
+  int escolha = menu(), numLinha1, numLinha2;
+  string conteudo;
+
+  while (escolha) {
+    system(CLEAR);
+    printTitle();
+    switch(escolha) {
+      case 1:
+        cout << "Digite o conteudo da nova linha: ";
+        cin.ignore();
+        getline(cin, conteudo);
+        inserir(&topo, &fim, conteudo);
+        break;
+      case 2:
+        cout << "Qual o numero da linha a ser excluida? ";
+        cin >> numLinha1;
+        excluir(&topo, &fim, numLinha1);
+        break;
+      case 3:
+        listar(topo, false);
+        break;
+      case 4:
+        listar(fim, true);
+        break;
+      case 5:
+        cout << "Qual o numero da linha a ser movida? ";
+        cin >> numLinha1;
+        cout << "Qual o numero da linha onde o trecho sera colocado? ";
+        cin >> numLinha2;
+        mover(&topo, &fim, numLinha1, numLinha2);
+        break;
+    }
+    escolha = menu();
+  }
+  
+  return 0;
+}
---
+++ Projetos/restaurante.cpp
@@ -0,0 +1,227 @@
+#include <iostream>
+
+using namespace std;
+
+#ifdef _WIN32
+  #define CLEAR "cls"
+#else
+  #define CLEAR "clear"
+#endif
+
+#define RESET "\033[0m"
+#define BOLD "\033[1m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define CYAN "\033[36m"
+
+struct noConsumo {
+  int codigo;
+  struct noConsumo* prox;
+};
+typedef struct noConsumo* noConPtr;
+
+struct noCliente {
+  int codigo;
+  struct noConsumo* topoConsumos;
+  struct noCliente* prox;
+  struct noCliente* ant;
+};
+typedef struct noCliente* noCliPtr;
+
+bool listaVazia(void* topo) {
+  return !topo;
+}
+
+void printTitle() {
+  printf(BOLD);
+  printf(CYAN);
+  printf("---- L'art Culinaire ----\n\n");
+  printf(RESET);
+}
+
+void saida() {
+  char caractere;
+  printf(RESET);
+  printf("\nDigite qualquer caractere para voltar ao menu ");
+  cin >> caractere;
+}
+
+void chegadaCliente(noCliPtr* topoEnd, noCliPtr* fimEnd) {
+  int codigo = listaVazia(*topoEnd) ? 100 : (*fimEnd)->codigo + 100;
+  noCliPtr novoNo = new noCliente;
+  novoNo->codigo = codigo;
+  novoNo->topoConsumos = NULL;
+  novoNo->ant = *fimEnd;
+  novoNo->prox = NULL;
+  if (listaVazia(*topoEnd)) {
+    *topoEnd = novoNo;
+  } else {
+    (*fimEnd)->prox = novoNo;
+  }
+  *fimEnd = novoNo;
+
+  printf(GREEN);
+  printf("Cliente de codigo %d foi adicionado com sucesso!\n", codigo);
+  saida();
+}
+
+void saidaCliente(noCliPtr* topoEnd, noCliPtr* fimEnd, int codigo) {
+  noCliPtr auxCli = *topoEnd;
+  noConPtr auxConsumo, temp;
+  int i = 0;
+
+  if (
+    codigo % 100 != 0 ||
+    codigo < 100 ||
+    (*fimEnd && codigo > (*fimEnd)->codigo)
+  ) {
+    printf(RED);
+    printf("\nNenhum cliente com o codigo %d foi encontrado.\n", codigo);
+    saida();
+    return;
+  }
+  
+  while (auxCli && auxCli->codigo != codigo) {
+    auxCli = auxCli->prox;
+  }
+
+  if (!auxCli) {
+    printf(RED);
+    printf("\nNenhum cliente com o codigo %d foi encontrado.\n", codigo);
+    saida();
+    return;
+  }
+
+  auxConsumo = auxCli->topoConsumos;
+  if (auxConsumo) printf("\nConsumos do cliente:\n");
+  while (auxConsumo) {
+    temp = auxConsumo;
+    printf("- Produto %d\n", auxConsumo->codigo);
+    auxConsumo = auxConsumo->prox;
+    delete temp;
+    i++;
+  }
+
+  if (auxCli->ant) auxCli->ant->prox = auxCli->prox;
+  if (auxCli->prox) auxCli->prox->ant = auxCli->ant;
+  if (auxCli == *topoEnd) *topoEnd = auxCli->prox;
+  if (auxCli == *fimEnd) *fimEnd = auxCli->ant;
+
+  delete auxCli;
+  
+  printf(GREEN);
+  printf("\nCliente de codigo %d liberado!\n", codigo);
+  saida();
+}
+
+void listarClientes(noCliPtr topo) {
+  if (listaVazia(topo)) {
+    printf(RED);
+    printf("Nao ha nenhum cliente na lista.\n");
+    saida();
+    return;
+  }
+
+  noCliPtr aux = topo;
+  noConPtr auxConsumo;
+  int i = 0;
+  while (aux) {
+    printf("%do cliente:\n", i+1);
+    printf("  Codigo: %d\n", aux->codigo);
+    if (aux->topoConsumos) {
+      auxConsumo = aux->topoConsumos;
+      printf("  Consumos:\n");
+      while (auxConsumo) {
+        printf("   - Produto %d\n", auxConsumo->codigo);
+        auxConsumo = auxConsumo->prox;
+      }
+    }
+    aux = aux->prox;
+    i++;
+  }
+
+  saida();
+}
+
+void consumoCliente(noCliPtr* topoEnd, noCliPtr* fimEnd, int codCliente, int codProduto) {
+  noCliPtr auxCli = *topoEnd;
+  noConPtr auxConsumo, novoNo = new noConsumo;
+
+  if (
+    codCliente % 100 != 0 ||
+    codCliente < 100 ||
+    (*fimEnd && codCliente > (*fimEnd)->codigo)
+  ) {
+    printf(RED);
+    printf("\nNenhum cliente com o codigo %d foi encontrado.\n", codCliente);
+    saida();
+    return;
+  }
+  
+  while (auxCli && auxCli->codigo != codCliente) {
+    auxCli = auxCli->prox;
+  }
+
+  if (!auxCli) {
+    printf(RED);
+    printf("\nNenhum cliente com o codigo %d foi encontrado.\n", codCliente);
+    saida();
+    return;
+  }
+
+  novoNo->prox = auxCli->topoConsumos;
+  novoNo->codigo = codProduto;
+  auxCli->topoConsumos = novoNo;
+
+  printf(GREEN);
+  printf("\nPedido realizado com sucesso!\n");
+  saida();
+}
+
+int menu() {
+  int escolha;
+
+  system(CLEAR);
+  printTitle();
+  cout << "[1] Chegada de cliente\n"
+       << "[2] Consumo de cliente\n"
+       << "[3] Saida de cliente\n"
+       << "[4] Listar clientes\n\n"
+       << "Qual acao deseja realizar? ";
+  cin >> escolha;
+
+  return escolha;
+}
+
+int main() {
+  noCliPtr topo = NULL, fim = NULL;
+  int escolha = menu(), codCliente, codProduto;
+
+  while (escolha >= 1 && escolha <= 4) {
+    system(CLEAR);
+    printTitle();
+    switch(escolha) {
+      case 1:
+        chegadaCliente(&topo, &fim);
+        break;
+      case 2:
+        cout << "Qual o codigo do cliente que realizou o consumo? ";
+        cin >> codCliente;
+        cout << "Qual o codigo do produto consumido? ";
+        cin >> codProduto;
+        consumoCliente(&topo, &fim, codCliente, codProduto);
+        break;
+      case 3:
+        cout << "Qual o codigo do cliente a ser liberado? ";
+        cin >> codCliente;
+        saidaCliente(&topo, &fim, codCliente);
+        break;
+      case 4:
+        listarClientes(topo);
+        break;
+    }
+    escolha = menu();
+  }
+  
+  return 0;
+}
